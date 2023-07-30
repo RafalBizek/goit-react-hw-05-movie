58628ea0022642f948e498c0516e6d86a5cada88
Rafał Biżek <49203973+RafalBizek@users.noreply.github.com>
2023-07-30 14:01:10 +0200

    fixbug

--- src/pages/movies/Movies.jsx
+++ src/pages/movies/Movies.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Cast from 'components/cast/Cast';
 import styles from './Movies.module.css';
 
