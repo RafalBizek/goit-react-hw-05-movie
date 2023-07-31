a4f2e7b3852c7e5ff5345c8d0fc4cc1f72b4e8da
Rafał Biżek <49203973+RafalBizek@users.noreply.github.com>
2023-07-31 19:11:56 +0200

    added loadcontext

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { LoadProvider } from 'components/loadcontext/LoadContext';
 import { App } from 'components/App'; // Upewnij się, że ścieżka do komponentu App jest poprawna
 import './index.css';
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <BrowserRouter basename="/goit-react-hw-05-movies/">
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies/">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <LoadProvider>
+      <BrowserRouter basename="/goit-react-hw-05-movies/">
+        <App />
+      </BrowserRouter>
+    </LoadProvider>
+  </React.StrictMode>
 );
