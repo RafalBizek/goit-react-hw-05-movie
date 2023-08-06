6a30f6f46999106b5df671f55029715e2981b1bd
Rafał Biżek <49203973+RafalBizek@users.noreply.github.com>
2023-08-06 08:16:54 +0200

    fix/refresh

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
-ReactDOM.render(
-  // <React.StrictMode>
-  <BrowserRouter basename="/goit-react-hw-05-movie/">
-    <App />
-  </BrowserRouter>,
-  // </React.StrictMode>
-  document.getElementById('root')
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement); // Używamy createRoot zamiast render
+root.render(
+  <React.StrictMode>
+    <BrowserRouter basename="/goit-react-hw-05-movie/">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
-// ss
